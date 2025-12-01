bacbbf09ef897597e3d8b9484717e0a310fd4f2f
Brynn Smith <brynn@genesisbox.ca>
2025-12-01 13:50:59 -0500

    Solved challenge #1

---
+++ day1-filterTheDefectiveGifts.js
@@ -0,0 +1,28 @@
+/* Challenge #1: Filter the Defective Gifts
+
+Santa has received a list of gifts, but some are defective. A gift is defective if its name contains the # character.
+
+Help Santa by writing a function that takes a list of gift names and returns a new list that only contains the non-defective gifts. */
+
+
+function filterGifts(gifts) {
+  return gifts.filter(gift => !gift.includes('#'));
+}
+
+
+// Examples
+
+const gifts1 = ['car', 'doll#arm', 'ball', '#train']
+const good1 = filterGifts(gifts1)
+console.log(good1)
+// ['car', 'ball']
+
+const gifts2 = ['#broken', '#rusty']
+const good2 = filterGifts(gifts2)
+console.log(good2)
+// []
+
+const gifts3 = []
+const good3 = filterGifts(gifts3)
+console.log(good3)
+// [] 
